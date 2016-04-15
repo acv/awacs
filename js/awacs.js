df0476a180e523bff6925e989d8fe915bd71a952
Alexandre Carmel-Veilleux <acv@miniguru.ca>
2016-04-15 10:57:05 -0300

    Air superiority track is in.

--- js/awacs.js
+++ js/awacs.js
@@ -14,14 +14,102 @@ var AwacsApp = function ($) {
   var adfDetection = 6;
   var adfSAM = 6;
   var adfAAA = 2;
+  var currentAirSup = "contested";
+  var validAirSup = {
+    'oppSupremacy': '#FF2000',
+    'oppSuperiority': '#FF3800',
+    'oppAdvantage': '#FF5000',
+    'contested': 'white',
+    'advantage': 'rgb(120, 170, 255)',
+    'superiority': 'rgb(120, 150, 255)',
+    'supremacy': 'rgb(120, 120, 255)'
+  };
   var currentPlayer = "Allied";
 
   var getPlayerColor = function () {
     if (currentPlayer === "Allied") {
       return "royalblue"
     } else {
-      return "darkred"
+      return "red"
     }
+  };
+
+  var getOtherPlayerColor = function () {
+    if (currentPlayer !== "Allied") {
+      return "royalblue"
+    } else {
+      return "red"
+    }
+  };
+
+  var airSupBoxColor = function (level) {
+    var color = validAirSup[level];
+    if (color === undefined) {
+      color = 'white'
+    }
+    return color;
+  };
+
+  var refreshAirSupTrack = function () {
+    $("#air-sup-boxes").find(".air-sup-box").each(function (index, element) {
+      var id = $(element).attr('id');
+      if (id === currentAirSup) {
+        $(element)
+          .css('background-color', 'black')
+          .css('color', 'white');
+      } else {
+        $(element)
+          .css('background-color', airSupBoxColor(id))
+          .css('color', 'black');
+      }
+    });
+  };
+
+  var airSupClickHandler = function (value) {
+    currentAirSup = value;
+    refreshAirSupTrack();
+  };
+
+  var initAirSupTrack = function () {
+    $("#air-sup-boxes").find(".air-sup-box").each(function (index, element) {
+      var id = $(element).attr('id');
+      $(element).on('click', function () {
+        airSupClickHandler(id);
+      });
+    });
+    refreshAirSupTrack();
+  };
+
+  var flipAirSupTrack = function () {
+    var airSup = currentAirSup;
+    if (airSup === 'contested') {
+      return;  // Skip.
+    }
+    if (airSup.substr(0, 3) === 'opp') {
+      airSup = airSup.substr(3).toLowerCase();
+    } else {
+      airSup = 'opp' + airSup.charAt(0).toUpperCase() + airSup.slice(1);
+    }
+    currentAirSup = airSup;
+    refreshAirSupTrack();
+  };
+
+  var flipPlayer = function () {
+    if (currentPlayer === 'Allied') {
+      currentPlayer = 'OpFor';
+    } else {
+      currentPlayer = 'Allied';
+    }
+    $("#flag-box").css("background-color", getPlayerColor());
+    flipAirSupTrack();
+  };
+
+  var initFlag = function () {
+    $("#flag-box")
+      .css('background-color', getPlayerColor())
+      .on('click', function () {
+        flipPlayer();
+      });
   };
 
   var refreshAdfTracks = function () {
@@ -30,20 +118,20 @@ var AwacsApp = function ($) {
       var value = parseInt(id[2]);
       if (id[0] === "det") {
         if (value === adfDetection) {
-          $(element).css('background-color', getPlayerColor());
+          $(element).css('background-color', getOtherPlayerColor());
         } else {
           $(element).css('background-color', "white");
         }
       } else if (id[0] === "sam") {
         if (value === adfSAM) {
-          $(element).css('background-color', getPlayerColor());
+          $(element).css('background-color', getOtherPlayerColor());
         } else {
           $(element).css('background-color', "white");
         }
 
       } else if (id[0] === 'aaa') {
         if (value === adfAAA) {
-          $(element).css('background-color', getPlayerColor());
+          $(element).css('background-color', getOtherPlayerColor());
         } else {
           $(element).css('background-color', "white");
         }
@@ -114,6 +202,8 @@ var AwacsApp = function ($) {
       }
     });
 
+    initFlag();
+    initAirSupTrack();
     initAdfTracks();
 
     if (selectedMode == null) {
